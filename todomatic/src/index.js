e32164dce3cc25a100ebb8fd75413723baf152c1
Ritaban Bandyopadhyay <ritabanbandyopadhyay@Ritabans-MacBook-Pro.local>
2022-02-15 15:14:33 +0530

    changes to App.js and index.js

--- todomatic/src/index.js
+++ todomatic/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App subject="Clarice" />,document.getElementById('root'));
 
 
